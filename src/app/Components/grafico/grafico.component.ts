1373abed39b880fa9c6c076324079ae723bb46ae
katabun124 <katabun124@gmail.com>
2024-11-03 18:25:03 +0000

    fourth-commit

--- src/app/Components/grafico/grafico.component.ts
+++ src/app/Components/grafico/grafico.component.ts
@@ -9,12 +9,35 @@ import { Component, Input } from '@angular/core';
   styleUrl: './grafico.component.css'
 })
 export class GraficoComponent {
-  @Input() vidaPokemon: number = 284;
-  @Input() defensaPokemon: number = 100;
-  @Input() ataquePokemon: number = 100;
-  @Input() idPokemon: string = "#001";
+  @Input() vidaPokemon: number = 0; // Initialize to 0
+  @Input() defensaPokemon: number = 0; // Initialize to 0
+  @Input() ataquePokemon: number = 0; // Initialize to 0
+  @Input() idPokemon: string = "#000";
   @Input() nombrePokemon: string = "Pokemon";
   @Input() tipoPokemon: string = "Normal";
+
+  showPercentage = {
+    hp: false,
+    attack: false,
+    defense: false
+  };
+  toggleDisplay2(stat: 'hp') {
+    this.showPercentage[stat] = !this.showPercentage[stat];
+  }
+
+  toggleDisplay(stat: 'attack' | 'defense') {
+    this.showPercentage[stat] = !this.showPercentage[stat];
+  }
+  calculateVida(value:number): number{
+    return (value/value)*100;
+  }
+  calculatePercentage(value: number, max: number): number {
+    if (max == 0) { 
+      return 0; // Avoid division by zero if max is 0
+    } else {
+    return parseFloat(((value / max) * 100).toFixed(1)); 
+    }
+  }
 
   capitalizeFirstLetter(string: string): string {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
--- src/app/Components/pokedex-view/pokedex-view.component.ts
+++ src/app/Components/pokedex-view/pokedex-view.component.ts
@@ -14,6 +14,13 @@ export class PokedexViewComponent {
   pokemonName: string = '';
   pokemonType: string = '';
   pokemonHP: number = 0;
+  pokemonAttack: number = 0;
+  pokemonDefense: number = 0;
+  maxStats = {
+    hp: 255, // Valor máximo de HP en la primera generación
+    attack: 190, // Valor máximo de Ataque
+    defense: 230 // Valor máximo de Defensa
+  };
   
   pokemonImage: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + this.pokemonID + '.png';
 
@@ -35,8 +42,11 @@ export class PokedexViewComponent {
         this.pokemonName = data.name;
         this.pokemonType = data.types[0].type.name;
         this.pokemonHP = data.stats[0].base_stat;
+        this.pokemonAttack = data.stats[1].base_stat;
+        this.pokemonDefense = data.stats[2].base_stat;
         // Emitir los nuevos datos al componente gráfico
         this.updateGraficoData();
+
       });
   }
 
